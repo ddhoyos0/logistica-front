3519df18c960ed8d20e85cc29dcf7770fc29fe48
Deymer de Jesús Hoyos Pérez <48861824+ddhoyos0@users.noreply.github.com>
2022-03-17 23:40:10 -0500

    completando crud de productos y despacho

---
+++ src/app/app/components/registrar/registrar.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {TokenService} from "../../core/servicios/token/token.service";
+import {AuthService} from "../../core/servicios/auth/auth.service";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
+import {Usuario} from "../../core/modelos/usuario";
+
+@Component({
+  selector: 'app-registrar',
+  templateUrl: './registrar.component.html',
+  styleUrls: ['./registrar.component.css']
+})
+export class RegistrarComponent implements OnInit {
+
+  nuevoUsuario!: Usuario;
+  nombre!: string;
+  nombreUsuario!: string;
+  email!: string;
+  password!: string;
+  errMsj!: string;
+  isLogged = false;
+
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    }
+  }
+
+  onRegister(): void {
+    this.nuevoUsuario = new Usuario(this.nombre, this.nombreUsuario, this.email, this.password);
+    this.authService.nuevo(this.nuevoUsuario).subscribe(
+      data => {
+        this.toastr.success('Cuenta Creada', 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+
+        this.router.navigate(['/login']);
+      },
+      err => {
+        this.errMsj = err.error.mensaje;
+        this.toastr.error(this.errMsj, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        // console.log(err.error.message);
+      }
+    );
+  }
+
+}
